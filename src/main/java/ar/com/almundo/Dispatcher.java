b443af0a344f69d0ff358f3c873ce47d768f00a4
Jorge Bo <Jorge.Bo@4finance.com>
2017-04-03 17:29:28 -0300

    use futures

--- src/main/java/ar/com/almundo/Dispatcher.java
+++ src/main/java/ar/com/almundo/Dispatcher.java
@@ -2,8 +2,12 @@ package ar.com.almundo;
 
 import java.util.Optional;
 import java.util.Queue;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,6 +15,9 @@ import net.jcip.annotations.ThreadSafe;
 
 @ThreadSafe
 class Dispatcher {
+
+    private static final long TEN_SECONDS = 10000000000L;
+
     private final Queue<Employee> availableEmployeesQueue;
     private final ExecutorService executorService;
     private final static Logger logger = Logger.getLogger(Dispatcher.class.getName());
@@ -22,14 +29,27 @@ class Dispatcher {
 
     public void dispatchCall(final Call call) {
         final Optional<Employee> employee = Optional.ofNullable(this.availableEmployeesQueue.poll());
+
         if (employee.isPresent()) {
-            this.executorService.execute(() -> handleCallToEmployee(call, employee.get()));
+            long endNanos = System.nanoTime() + TEN_SECONDS;
+            final Future<Employee> future = this.executorService.submit(() -> handleCallToEmployee(call, employee.get()));
+            long timeLeft = endNanos - System.nanoTime();
+            try {
+                future.get(timeLeft, TimeUnit.NANOSECONDS);
+            } catch (InterruptedException | ExecutionException e) {
+                logger.log(Level.SEVERE, "Execution exception", e);
+            } catch (final TimeoutException e) {
+                future.cancel(true);
+                logger.log(Level.WARNING, "Call exceded maximum allow timeout", e);
+            } finally {
+                availableEmployeesQueue.add(employee.get());
+            }
         } else {
             handleNoEmployeesAvailable();
         }
     }
 
-    private void handleCallToEmployee(final Call call, final Employee employee) {
+    private Employee handleCallToEmployee(final Call call, final Employee employee) {
         employee.assignCall(call);
 
         //employee.sayHello('Hello')
@@ -43,7 +63,7 @@ class Dispatcher {
         //employee.sayGoodbye("Bye")
         logger.log(Level.INFO, "Employee stopping conversation with client...:(!");
 
-        availableEmployeesQueue.add(employee);
+        return employee;
     }
 
     private void randomDelay(float min, float max) {
